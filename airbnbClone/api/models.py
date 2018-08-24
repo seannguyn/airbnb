b0000933caaa4297363c881a670b2b9f22331529
seannguyn <seannguyen5696@gmail.com>
2018-08-24 16:37:56 +1000

    no change

--- airbnbClone/api/models.py
+++ airbnbClone/api/models.py
@@ -8,9 +8,7 @@ import datetime
 
 class UserInfo(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    location = models.CharField(max_length=30, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-    
+
     def __str__(self):
         return self.user.username
 
