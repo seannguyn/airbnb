534e4ffea1300d9175273098e3b8c064191f9d55
NAMTRAN\nam tran <tdnam39@gmail.com>
2018-08-30 13:43:15 +1000

    grid

--- frontend/src/Component/Review/Review.js
+++ frontend/src/Component/Review/Review.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Review extends Component {
     state = {  }
-    render() { 
-        return ( 
-            <div>
-                                
-            </div>
-         );
+
+    async componentDidMount(){
+        console.log("DIDMOUTNREVIEW: ", this.props);
+        const res = await axios.get("https://localhost:8000/users/1/");
+        console.log("RES: ", res.data);
     }
-}
- 
+
+    render() {
+        console.log("GO TO REVIEW");
+        console.log("props reivw: ", this.props)
+        const {text, closeReview, star, review, user} = this.props;
+        return (
+            <React.Fragment>
+
+                    <div className="container" style={{background:"#f7f7f7"}}>
+                    Rating {star}
+                    <p className><b>{review}  <i>(by {user})</i></b></p> 
+                    </div>
+
+            </React.Fragment>
+        );
+      }
+    }
+
 export default Review;
