7938c20ffcde8485c0822a2a705c30fe62316f98
phoBowl <tdnam39@gmail.com>
2018-09-12 02:51:32 +1000

    fix

--- frontend/src/Component/Function/Hosting.js
+++ frontend/src/Component/Function/Hosting.js
@@ -4,12 +4,8 @@ import {Link} from 'react-router-dom';
 import Review from '../Review/Review';
 
 import { withStyles } from '@material-ui/core/styles';
-// import Card from '@material-ui/core/Card';
-// // import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Cloud from "@material-ui/icons/Cloud";
 import FormatQuote from "@material-ui/icons/FormatQuote";
@@ -183,7 +179,6 @@ class Hosting extends React.Component {
                   readonly={readonly}
                   initialRating={0}
               />
-
             }
             {this.state.seeReviews ?
               <CardContent>{ reviews.map(review =>
@@ -201,7 +196,6 @@ class Hosting extends React.Component {
               </CardContent>
               : null
             }
-
           </CardBody>
           <CardFooter product>
               <Link to={`/accommodations/${id}`}>
