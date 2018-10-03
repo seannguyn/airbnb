6214a68180c85a47045d69520a054cf4824d9768
ZahidMd <z.binmohammed@student.unsw.edu.au>
2018-10-03 23:49:54 +1000

    edits to formatting

--- airbnbClone/api/models.py
+++ airbnbClone/api/models.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 import uuid
 import datetime
 
+
 # Create your models here.
 
 class UserInfo(models.Model):
@@ -13,8 +14,8 @@ class UserInfo(models.Model):
     def __str__(self):
         return self.user.username
 
-class Accommodation(models.Model):
 
+class Accommodation(models.Model):
     ACCOMMODATION_TYPES = (
         ('Room', 'Room'),
         ('Studio', 'Studio'),
@@ -30,50 +31,52 @@ class Accommodation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Accomodation_Type = models.CharField(max_length=10, choices=ACCOMMODATION_TYPES)
 
-    title = models.CharField(blank=False, max_length=100,default=uuid.uuid4())
+    title = models.CharField(blank=False, max_length=100, default=uuid.uuid4())
 
     addr_number = models.PositiveIntegerField(blank=False, default=1)
-    addr_street = models.CharField(blank=False, max_length=100,default='dedault')
-    addr_city   = models.CharField(blank=False, max_length=100,default='default')
-    addr_state  = models.CharField(max_length=10, choices=STATES,default='NSW')
+    addr_street = models.CharField(blank=False, max_length=100, default='dedault')
+    addr_city = models.CharField(blank=False, max_length=100, default='default')
+    addr_state = models.CharField(max_length=10, choices=STATES, default='NSW')
 
-    area            = models.IntegerField(default=1, blank=False)
-    bedroom_master  = models.IntegerField(default=1, blank=False)
-    bedroom         = models.IntegerField(default=1, blank=False)
-    bathroom        = models.IntegerField(default=1, blank=False)
-    kitchen         = models.IntegerField(default=1, blank=False)
+    area = models.IntegerField(default=1, blank=False)
+    bedroom_master = models.IntegerField(default=1, blank=False)
+    bedroom = models.IntegerField(default=1, blank=False)
+    bathroom = models.IntegerField(default=1, blank=False)
+    kitchen = models.IntegerField(default=1, blank=False)
 
+    pool = models.IntegerField(default=0, blank=False)
+    gym = models.IntegerField(default=0, blank=False)
+    carpark = models.IntegerField(default=0, blank=False)
 
-    pool            = models.IntegerField(default=0, blank=False)
-    gym             = models.IntegerField(default=0, blank=False)
-    carpark         = models.IntegerField(default=0, blank=False)
-
-    description     = models.TextField(blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
-        return self.user.username + "_"+self.Accomodation_Type+"_" + str(self.id)
+        return self.user.username + "_" + self.Accomodation_Type + "_" + str(self.id)
+
 
 class AccommodationImage(models.Model):
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
-    url_height=models.PositiveIntegerField(default='500')
-    url_width=models.PositiveIntegerField(default='500')
+    url_height = models.PositiveIntegerField(default='500')
+    url_width = models.PositiveIntegerField(default='500')
     a_image = models.ImageField(upload_to="accommodation/", height_field='url_height', width_field='url_width')
 
     def save(self, *args, **kwargs):
         return super(AccommodationImage, self).save(*args, **kwargs)
+
 
 class AccommodationHosting(models.Model):
     accommodation = models.OneToOneField(Accommodation, on_delete=models.CASCADE)
     date_start = models.DateField(default=datetime.datetime.today)
     date_end = models.DateField(default=datetime.datetime.today)
     guest = models.IntegerField(blank=False, default=1)
-    check_in = models.TimeField(blank=True,default=datetime.time(14,30,0,0))
-    check_out = models.TimeField(blank=True,default=datetime.time(10,0,0,0))
+    check_in = models.TimeField(blank=True, default=datetime.time(14, 30, 0, 0))
+    check_out = models.TimeField(blank=True, default=datetime.time(10, 0, 0, 0))
     price = models.PositiveIntegerField(blank=False)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return self.accommodation.__str__() + "_" + str(self.date_start) + "_" + str(self.date_end);
+
 
 class AccommodationHostingArchive(models.Model):
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
@@ -82,8 +85,8 @@ class AccommodationHostingArchive(models.Model):
     price = models.PositiveIntegerField(blank=False)
     description = models.TextField(blank=True)
 
-class Booking(models.Model):
 
+class Booking(models.Model):
     hosting = models.ForeignKey(AccommodationHosting, on_delete=models.CASCADE)
     booker = models.ForeignKey(User, on_delete=models.CASCADE)
     isPaid = models.BooleanField(default=False)
@@ -92,10 +95,10 @@ class Booking(models.Model):
     date_end = models.DateField(default=datetime.datetime.today)
     date_paymentDue = models.DateField(default=datetime.datetime.today)
 
-    note  = models.TextField(blank=True)
+    note = models.TextField(blank=True)
+
 
 class Review(models.Model):
-
     SCALE = (
         (0, 0),
         (1, 1),
@@ -103,19 +106,19 @@ class Review(models.Model):
         (3, 3),
         (4, 4),
         (5, 5)
-        )
+    )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
     star = models.IntegerField(choices=SCALE)
     review = models.TextField(blank=True)
-    date_posted = models.DateField(default=datetime.datetime.today,blank=True)
+    date_posted = models.DateField(default=datetime.datetime.today, blank=True)
+
 
 class Search(models.Model):
-
     accommodation = models.OneToOneField(Accommodation, on_delete=models.CASCADE, primary_key="true")
     date_free = models.TextField(blank=False)
-    price = models.PositiveIntegerField(blank=False,default=100)
+    price = models.PositiveIntegerField(blank=False, default=100)
     guest = models.IntegerField(blank=False, default=1)
-    location = models.CharField(blank=False, max_length=100,default='default')
+    location = models.CharField(blank=False, max_length=100, default='default')
