2c5e7ae1865d7c0113a7f4b01bfbbaa74286a75e
Sean Nguyen <seannguyen5696@gmail.com>
2018-10-04 18:45:53 +1000

    reply

---
+++ frontend/src/Component/Review/ReplyForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+export default class FormDialog extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <Dialog
+          open={true}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Reply</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Reply"
+              type="email"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.props.close} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.props.close} color="primary">
+              Reply
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
--- frontend/src/Component/Review/ReviewRow.js
+++ frontend/src/Component/Review/ReviewRow.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import PersonIcon from '@material-ui/icons/Person';
+import {Consumer} from '../../Context';
+import ReplyForm from './ReplyForm';
 
 class ReviewRow extends React.Component {
+
+  state = {
+    reply: false
+  }
+
+  openReply = () => {
+    this.setState({
+      reply: true
+    })
+  }
+
+  closeReply = () => {
+    this.setState({
+      reply: false
+    })
+  }
+
   render () {
-
-
     const monthNames = ["January", "Febuary", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
 ];
@@ -16,30 +34,38 @@ class ReviewRow extends React.Component {
     const month_year = month + " " + year
 
     return (
-      <div>
-        <div style={{marginTop:'15px',marginLeft:'15px'}}>
-          <div className="row">
-            <div className="col-1">
-              <Avatar>
-                <PersonIcon/>
-              </Avatar>
-            </div>
-            <div className="col-11">
-              <Typography variant="subheading">{this.props.review.user}</Typography>
-              <Typography variant="caption">{month_year}</Typography>
-            </div>
-          </div>
-        </div>
+      <Consumer>
+        {value => {
+          return (
+            <div>
+              <div style={{marginTop:'15px',marginLeft:'15px'}}>
+                <div className="row">
+                  <div className="col-1">
+                    <Avatar>
+                      <PersonIcon/>
+                    </Avatar>
+                  </div>
+                  <div className="col-11">
+                    <Typography variant="subheading">{this.props.review.user}</Typography>
+                    <Typography variant="caption">{month_year}</Typography>
+                  </div>
+                </div>
+              </div>
 
-        <div style={{marginTop:'10px',marginLeft:'10px'}}>
-          <div className="row">
-            <div className="col-12">
-              <p>{this.props.review.review}</p>
+              <div style={{marginTop:'10px',marginLeft:'10px'}}>
+                <div className="row">
+                  <div className="col-12">
+                    <p>{this.props.review.review}</p>
+                  </div>
+                </div>
+              </div>
+              <Button color="secondary" onClick={this.openReply}> Reply </Button>
+              {this.state.reply === true && (<ReplyForm close={this.closeReply}/>)}
             </div>
-          </div>
-        </div>
+          )
+        }}
+      </Consumer>
 
-      </div>
     )
   }
 }
