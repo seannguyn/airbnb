b508fde79d7ea18fc477e490036c8ed9e85d959a
ZahidMd <z.binmohammed@student.unsw.edu.au>
2018-10-10 21:31:21 +1100

    username now shows on reviews

--- airbnbClone/api/models.py
+++ airbnbClone/api/models.py
@@ -1,9 +1,9 @@
-from django.db import models
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.models import User
-from rest_framework import serializers
-import uuid
 import datetime
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
 
 # Create your models here.
 
@@ -13,8 +13,8 @@ class UserInfo(models.Model):
     def __str__(self):
         return self.user.username
 
-class Accommodation(models.Model):
 
+class Accommodation(models.Model):
     ACCOMMODATION_TYPES = (
         ('Room', 'Room'),
         ('Studio', 'Studio'),
@@ -30,48 +30,50 @@ class Accommodation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     Accomodation_Type = models.CharField(max_length=10, choices=ACCOMMODATION_TYPES)
 
-    title = models.CharField(blank=False, max_length=100,default=uuid.uuid4())
+    title = models.CharField(blank=False, max_length=100, default=uuid.uuid4())
 
     address = models.CharField(blank=False, max_length=300, default='default address')
     latitude = models.FloatField(blank=False, default=1)
     longitude = models.FloatField(blank=False, default=1)
 
-    bed             = models.IntegerField(default=1, blank=False)
-    bedroom         = models.IntegerField(default=1, blank=False)
-    bathroom        = models.IntegerField(default=1, blank=False)
+    bed = models.IntegerField(default=1, blank=False)
+    bedroom = models.IntegerField(default=1, blank=False)
+    bathroom = models.IntegerField(default=1, blank=False)
 
-    kitchen         = models.BooleanField(default=False,blank=False)
-    gym             = models.BooleanField(default=False,blank=False)
-    pool            = models.BooleanField(default=False,blank=False)
-    carpark         = models.BooleanField(default=False,blank=False)
+    kitchen = models.BooleanField(default=False, blank=False)
+    gym = models.BooleanField(default=False, blank=False)
+    pool = models.BooleanField(default=False, blank=False)
+    carpark = models.BooleanField(default=False, blank=False)
 
-
-    description     = models.TextField(blank=True)
+    description = models.TextField(blank=True)
 
     def __str__(self):
-        return self.user.username + "_"+self.Accomodation_Type+"_" + str(self.id)
+        return self.user.username + "_" + self.Accomodation_Type + "_" + str(self.id)
+
 
 class AccommodationImage(models.Model):
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
-    url_height=models.PositiveIntegerField(default='500')
-    url_width=models.PositiveIntegerField(default='500')
+    url_height = models.PositiveIntegerField(default='500')
+    url_width = models.PositiveIntegerField(default='500')
     a_image = models.ImageField(upload_to="accommodation/", height_field='url_height', width_field='url_width')
 
     def save(self, *args, **kwargs):
         return super(AccommodationImage, self).save(*args, **kwargs)
+
 
 class AccommodationHosting(models.Model):
     accommodation = models.OneToOneField(Accommodation, on_delete=models.CASCADE)
     date_start = models.DateField(default=datetime.datetime.today)
     date_end = models.DateField(default=datetime.datetime.today)
     guest = models.IntegerField(blank=False, default=1)
-    check_in = models.TimeField(blank=True,default=datetime.time(14,30,0,0))
-    check_out = models.TimeField(blank=True,default=datetime.time(10,0,0,0))
+    check_in = models.TimeField(blank=True, default=datetime.time(14, 30, 0, 0))
+    check_out = models.TimeField(blank=True, default=datetime.time(10, 0, 0, 0))
     price = models.PositiveIntegerField(blank=False)
     description = models.TextField(blank=True)
 
     def __str__(self):
         return self.accommodation.__str__() + "_" + str(self.date_start) + "_" + str(self.date_end);
+
 
 class AccommodationHostingArchive(models.Model):
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
@@ -80,8 +82,8 @@ class AccommodationHostingArchive(models.Model):
     price = models.PositiveIntegerField(blank=False)
     description = models.TextField(blank=True)
 
-class Booking(models.Model):
 
+class Booking(models.Model):
     hosting = models.ForeignKey(AccommodationHosting, on_delete=models.CASCADE)
     booker = models.ForeignKey(User, on_delete=models.CASCADE)
     isPaid = models.BooleanField(default=False)
@@ -90,10 +92,10 @@ class Booking(models.Model):
     date_end = models.DateField(default=datetime.datetime.today)
     date_paymentDue = models.DateField(default=datetime.datetime.today)
 
-    note  = models.TextField(blank=True)
+    note = models.TextField(blank=True)
+
 
 class Review(models.Model):
-
     SCALE = (
         (0, 0),
         (1, 1),
@@ -101,34 +103,34 @@ class Review(models.Model):
         (3, 3),
         (4, 4),
         (5, 5)
-        )
+    )
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     accommodation = models.ForeignKey(Accommodation, on_delete=models.CASCADE)
     booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
     star = models.IntegerField(choices=SCALE)
     review = models.TextField(blank=True)
-    date_posted = models.DateField(default=datetime.datetime.today,blank=True)
+    date_posted = models.DateField(default=datetime.datetime.today, blank=True)
+
 
 class Search(models.Model):
-
     accommodation = models.OneToOneField(Accommodation, on_delete=models.CASCADE, primary_key="true")
     date_free = models.TextField(blank=False)
-    price = models.PositiveIntegerField(blank=False,default=100)
+    price = models.PositiveIntegerField(blank=False, default=100)
     guest = models.IntegerField(blank=False, default=1)
-    location = models.CharField(blank=False, max_length=100,default='default')
+    location = models.CharField(blank=False, max_length=100, default='default')
+
 
 class ReviewCount(models.Model):
-
     accommodation = models.OneToOneField(Accommodation, on_delete=models.CASCADE, primary_key="true")
     count = models.IntegerField(blank=False, default=0)
 
-class BookRequest(models.Model):
 
-    title       = models.TextField(blank=False)
-    date        = models.TextField(blank=True)
-    content     = models.TextField(blank=False)
-    sender      = models.TextField(blank=True)
-    toHost      = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
-    hasReply    = models.BooleanField(default=False)
-    reply       = models.TextField(blank=True)
+class BookRequest(models.Model):
+    title = models.TextField(blank=False)
+    date = models.TextField(blank=True)
+    content = models.TextField(blank=False)
+    sender = models.TextField(blank=True)
+    toHost = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    hasReply = models.BooleanField(default=False)
+    reply = models.TextField(blank=True)
--- airbnbClone/api/serializers.py
+++ airbnbClone/api/serializers.py
@@ -1,64 +1,76 @@
-from rest_framework import serializers
-from rest_framework.validators import UniqueValidator
-from django.contrib.auth.models import User
-from api.models import Accommodation, AccommodationImage, AccommodationHosting, Booking, Review, UserInfo, Search, ReviewCount, BookRequest
-from pymongo import MongoClient
 import smtplib
 
+from api.models import Accommodation, AccommodationImage, AccommodationHosting, Booking, Review, UserInfo, Search, \
+    ReviewCount, BookRequest
+from pymongo import MongoClient
+from rest_framework import serializers
 
 # set up mongodb
 MONGODB_URI = "mongodb://comp3900:comp3900@ds125293.mlab.com:25293/comp3900"
 client = MongoClient(MONGODB_URI, connectTimeoutMS=30000)
 db = client.get_database("comp3900")
 
-class AccommodationSerializer(serializers.ModelSerializer):
 
+class AccommodationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Accommodation
-        fields = ('id','user','title', 'address', 'latitude', 'longitude',
-                    'Accomodation_Type','bed','bedroom',
-                    'kitchen','bathroom','pool',
-                    'gym','carpark','description')
+        fields = ('id', 'user', 'title', 'address', 'latitude', 'longitude',
+                  'Accomodation_Type', 'bed', 'bedroom',
+                  'kitchen', 'bathroom', 'pool',
+                  'gym', 'carpark', 'description')
+
 
 class AccommodationImageSerializer(serializers.ModelSerializer):
-
     a_image = serializers.ImageField(max_length=None, allow_empty_file=False, use_url=True)
 
     class Meta:
         model = AccommodationImage
-        fields = ('id','accommodation','url_height','url_width','a_image')
+        fields = ('id', 'accommodation', 'url_height', 'url_width', 'a_image')
+
     def create(self, validated_data):
         return AccommodationImage.objects.create(**validated_data)
+
 
 class AccommodationHostingSerializer(serializers.ModelSerializer):
     class Meta:
         model = AccommodationHosting
-        fields = ('id', 'accommodation','date_start','date_end','check_in','check_out','price','guest','description')
+        fields = (
+        'id', 'accommodation', 'date_start', 'date_end', 'check_in', 'check_out', 'price', 'guest', 'description')
+
 
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ('id','isPaid','guest','hosting','booker','date_start','date_end','date_paymentDue','note')
+        fields = ('id', 'isPaid', 'guest', 'hosting', 'booker', 'date_start', 'date_end', 'date_paymentDue', 'note')
+
 
 class ReviewSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True, source='user.username')
+
     class Meta:
         model = Review
-        fields = ('id', 'user', 'accommodation', 'booking', 'star', 'review', 'date_posted')
+        fields = ('id', 'user', 'username', 'accommodation', 'booking', 'star', 'review', 'date_posted')
+
 
 class UserInfoSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(read_only=True, source='user.username')
+
     class Meta:
         model = UserInfo
-        fields = ('id', 'user')
+        fields = ('id', 'user', 'username')
+
 
 class SearchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Search
         fields = ('accommodation', 'date_free', 'price', 'guest', 'location')
 
+
 class ReviewCountSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReviewCount
         fields = ('accommodation', 'count')
+
 
 class BookRequestSerializer(serializers.ModelSerializer):
     class Meta:
@@ -66,7 +78,6 @@ class BookRequestSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def update(self, instance, validated_data):
-
         instance.id = validated_data.get('id', instance.id)
         instance.title = validated_data.get('title', instance.title)
         instance.date = validated_data.get('date', instance.date)
@@ -77,7 +88,7 @@ class BookRequestSerializer(serializers.ModelSerializer):
         instance.toHost = validated_data.get('toHost', instance.toHost)
 
         # EMAIL SENDING
-        systemEmail = db['portBnB'].find_one( { "email" : "comp3900project3@gmail.com" } )
+        systemEmail = db['portBnB'].find_one({"email": "comp3900project3@gmail.com"})
 
         message = 'Subject: {}\n\n{}'.format(instance.title, instance.reply)
 
