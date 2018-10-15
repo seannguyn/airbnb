cafc72a7771c28564cd7fa0130d2894ecc6d85cd
Sean Nguyen <seannguyen5696@gmail.com>
2018-10-16 06:31:30 +1100

    fix style lil bit

--- frontend/src/Component/Contexts/AllHostingContext.js
+++ frontend/src/Component/Contexts/AllHostingContext.js
@@ -40,9 +40,9 @@ class AllHostingContext extends React.Component {
             )
           } else {
             rendering.push(
-              <h1 key="map">
+              <div className="container">
                 <Map/>
-              </h1>
+              </div>
             )
           }
 
